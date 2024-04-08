dd2b739a4de6183863e1a9a32aa735ad19be1f71
saikat-iit <sd.saikat369@gmail.com>
2024-04-09 04:03:36 +0530

    improved image rendering

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatefulWidget {
   final List wallpaperID;
@@ -41,52 +44,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: <Widget>[
-      Expanded(
-        child: MasonryGridView.builder(
-            itemCount: wallpaperID.length,
-            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2),
-            itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: ClipRRect(
+      body: Stack(children: <Widget>[
+        Expanded(
+            child: MasonryGridView.builder(
+          itemCount: wallpaperID.length,
+          gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                elevation: 30,
+                shadowColor: Colors.black,
+                child: ClipRRect(
                     borderRadius: BorderRadius.circular(12),
-                    child: Material(
-                      elevation: 30,
-                      child: Image(image:  Image.network(widget.urls[index]).image)),
-                  ),
-                )),
-      )
-    ]),
-    backgroundColor: Colors.pink.shade100,
+                    child: CachedNetworkImage(
+                      imageUrl: widget.urls[index],
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) => Lottie.asset(
+                              "assets/animation/cat_paw_loading.json",
+                              repeat: true,
+                              width: 150),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    )
+                    // Image(image:  Image.network(widget.urls[index]).image)),
+                    ),
+              )),
+        ))
+      ]),
+      backgroundColor: Colors.pink.shade100,
     );
   }
-
-  // Image getWallpaper(List urls, int index) {
-  //   var data = widget.data[wallpaperID[index]];
-  //   List resolution = data['resolution'].split("x");
-  //   try {
-  //     int width = int.parse(resolution[0]);
-  //     int height = int.parse(resolution[1]);
-
-  //     Image image = Image(
-  //       image: Image.network(
-  //         urls[index],
-  //         width: width as double,
-  //         cacheWidth: width ~/ 5,
-  //         height: height as double,
-  //         cacheHeight: height ~/ 5,
-  //       ).image,
-  //     );
-  //     print("Successful !!!!");
-  //     return image;
-  //   } on Exception catch (_) {
-  //     Image image = Image(
-  //       image: Image.network(
-  //         urls[index],
-  //       ).image,
-  //     );
-  //     return image;
-  //   }
-  // }
 }
